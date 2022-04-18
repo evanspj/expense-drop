1bef2663f06a931d4e4b432b8e8ddf4ca69cf750
evanspj <8595624+evanspj@users.noreply.github.com>
2022-04-18 14:56:11 -0500

    feat: Add seo meta tags

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -26,6 +26,30 @@ class MyDocument extends Document {
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" content="#ffffff" />
+          <meta content="A privacy-focused way to review your expenses." />
+          <meta property="og:url" content="https://expense-drop.vercel.app/" />
+          <meta property="og:type" content="website" />
+          <meta property="og:site_name" content="expense drop" />
+          <meta
+            property="og:description"
+            content="A privacy-focused way to review your expenses. All file processing and data stays in your browser. No data is stored on a server. Your important financial data should be for your eyes only."
+          />
+          <meta property="og:title" content="Expense Drop" />
+          <meta
+            property="og:image"
+            content="https://expense-drop.vercel.app/expense_drop_social_share-optimized.png"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@_evanspj" />
+          <meta name="twitter:title" content="Expense Drop" />
+          <meta
+            name="twitter:description"
+            content="A privacy-focused way to review your expenses. All file processing and data stays in your browser. No data is stored on a server. Your important financial data should be for your eyes only."
+          />
+          <meta
+            name="twitter:image"
+            content="https://expense-drop.vercel.app/expense_drop_social_share-optimized.png"
+          />
         </Head>
         <body className="w-full flex bg-gray-100 text-gray-800 antialiased">
           <Main />
